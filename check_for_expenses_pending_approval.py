0bde2509f53e4cde5e7dbfa4371a0edd1a63ccdd
Gabriel Dias Vilela <bielvileladias1@gmail.com>
2024-08-21 18:53:42 -0300

    adding .env example

--- check_for_expenses_pending_approval.py
+++ check_for_expenses_pending_approval.py
@@ -1,13 +1,13 @@
 from abstra.workflows import *
 from abstra.tables import *
-
 from texts import thread_title_pending_approval
+import os
 
 
 EXPENSES_TABLE = 'expenses'
 expenses = select(EXPENSES_TABLE, where={'approval_status': None})
 
-finance_email = 'sample@domain.com'
+finance_email = os.environ["FINANCE_TEAM_EMAIL"]
                  
 if expenses:
     set_data('finance_email', finance_email)
